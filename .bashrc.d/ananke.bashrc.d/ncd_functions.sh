15b850bdc9322574dc8a3e447d24cd17a523e2e6
phillmac <phillmac@users.noreply.github.com>
2020-07-30 12:17:19 +1000

    Add ncd_functions

---
+++ .bashrc.d/ananke.bashrc.d/ncd_functions.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+alias sshp="ssh -p35681"
+
+function ncd_push () {
+    (cd /d/Source/Phill/Repos/PVTE/NCDSprinkle \
+    && git pull \
+    && git push \
+    && sshp kore docker exec -u www-data pvte-dev_ncd_dev_1 bash -c "'cd app/sprinkles/ncd && git pull'")
+}
+
+
+function ncd_reset() {
+  (cd /d/Source/Phill/Repos/PVTE/NCDSprinkle \
+   && git reset --hard
+   )
+}
+
+
+function start_release () {
+version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+  else
+    git flow release start "${version_tag}"
+  fi
+}
+
+function finish_release () {
+  version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+    return 252
+  fi
+  git flow release finish "${version_tag}" --message "Version ${version_tag}"
+}
+
+function build_ncd_image () {
+  version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+    return 252
+  fi
+  sshp kore bash -c "'source ~/.bashrc.d/dockerenv && build_pvte_image ncd --no-cache --tag 991291726468.dkr.ecr.ap-southeast-2.amazonaws.com/pvte/ncd:latest --tag 991291726468.dkr.ecr.ap-southeast-2.amazonaws.com/pvte/ncd:${version_tag}'"
+}
+
+function ncd_release () {
+  version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+  else
+    (cd /d/Source/Phill/Repos/PVTE/NCDSprinkle \
+    && start_release "${version_tag}" \
+    && finish_release "${version_tag}"
+    ) \
+    && ncd_push \
+    && build_ncd_image "${version_tag}"
+  fi
+}
+
+function ncd_publish () {
+  version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+    return 252
+  fi
+  sshp kore bash -c "'source ~/.bashrc.d/dockerenv && aws_publish pvte/ncd ${version_tag}'"
+}
+
+function ncd_deploy () {
+    sshp kore bash -c "'source ~/.bashrc.d/dockerenv && pvte_prod_service_deploy_hughes ncd'"
+}
