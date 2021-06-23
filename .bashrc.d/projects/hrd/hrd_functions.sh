a81aec4d21a26513179a7f50d7e5fee8db4616fc
Phillip Mackintosh <phill@peelvalley.com.au>
2021-06-23 15:30:54 +1000

    Fix build_hrd_image tags

--- .bashrc.d/projects/hrd/hrd_functions.sh
+++ .bashrc.d/projects/hrd/hrd_functions.sh
@@ -8,4 +8,11 @@ function hrd_push () {
     && sshp phill@kore.peelvalley.com.au docker exec -u www-data epra-dev_hrd_dev_1 bash -c "'cd app/sprinkles/hrd && git fetch && git checkout origin/$(git branch --show-current)'")
 }
 
-
+function build_hrd_image () {
+  version_tag=$1
+  if [[ -z "${version_tag}" ]]; then
+    echo "Version tag is required"
+    return 252
+  fi
+  sshp phill@kore.peelvalley.com.au bash -c "'source ~/.bashrc.d/dockerenv && build_epra_image hrd --no-cache --tag epra/hrd:${version_tag}'"
+}
